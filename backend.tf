4221c92f29693c51cbbbaca70c6b4a424348d70f
yong-netops <103978132+yong-netops@users.noreply.github.com>
2022-04-19 17:45:08 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,14 +1,5 @@
-# terraform {
-#   required_version = ">= 1.1.0"
-#   backend "remote" {
-#     organization = "Yong-2022"
-#     workspaces {
-#       name = "ace-iac-day-two"
-#     }
-#   }
-# }
-
 terraform {
+  required_version = ">= 1.1.0"
   cloud {
     organization = "Yong-2022"
 
